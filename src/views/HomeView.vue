ab59fe67de5c7829346f1ab5ce59027cc0546846
IzerGalai <IzerGalai>
2022-04-20 12:31:34 +0800

    add getting news from server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
     <div class="news-items">
-        <news-item
-          v-for="(i, k) in news" :key="k" :title="i.title" :img="i.img"
-          :datetime="i.dt" :description="i.desc" :tag="i.tag" :source="i.source"
-          />
+      <news-item
+        v-for="(i, k) in news" :key="k"
+        :title="i.attributes.name"  :datetime="i.attributes.createdAt" :description="i.attributes.description"
+        :img="i.attributes.preview_img.data ? 'http://localhost:1337' + i.attributes.preview_img.data.attributes.url : ''"
+        :tag="i.attributes.tags.data ? i.attributes.tags.data[0].attributes.name : ''"
+        :source="i.attributes.source.data ? i.attributes.source.data.attributes.name : ''"
+        />
+      <div v-if="this.pageCount > 1">
         <paginate
-          :page-count="8"
+          :page-count="pageCount"
           :click-handler="clickPageH"
           :prev-text="'Prev'"
           :next-text="'Next'"
           :container-class="'pagination'">
         </paginate>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +24,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import NewsItem from '@/components/NewsItem.vue'; // @ is an alias to /src
+import axios from 'axios'
+import qs from 'qs';
 
 export default Vue.extend({
   name: 'HomeView',
@@ -26,35 +33,56 @@ export default Vue.extend({
     NewsItem,
   },
   data () {
-    let news = [
-      {
-        title: "Криптозима уже наступила». Как обваливалась цена биткоина и что дальше",
-        img: '/news-item1.png',
-        dt: 1629184013000,
-        desc: `Биткоин подешевел на 40% с исторического максимума. Эксперты рассказали, стоит ли ждать повторения сценария 2018 года, когда криптовалюты потеряли в цене 80-90%.
-13 апреля биткоин торгуется по $41 тыс. В ноябре прошлого года криптовалюта установила исторический максимум на отметке в $69 тыс. С тех пор она подешевела на 40%.
-Период, когда котировки криптовалют падают в течение длительного времени, называется криптозимой. Также она сигнализирует о падении интереса к сфере цифровых активов.`,
-        tag: 'BTC',
-        source: 'РБК',
-      },
-      {
-        title: "Названа особенность будущих процессоров Intel",
-        img: '/news-item2.png',
-        dt: Date.now() - 3654000,
-        desc: `Следующее поколение процессоров Intel для компьютеров будут работать с повышенной частотой. Об этом сообщает портал WCCF Tech со ссылкой на собственный источник в отрасли.
-Речь идет о чипах 13-го поколения, которые получат название Raptor Lake. В них частота больших ядер может возрасти примерно на 300 МГц относительно процессоров текущей серии. То есть, условный флагман линейки i9 вплотную приблизится к рекордным 6 ГГц.`,
-        tag: 'Intel',
-        source: 'WCCF Tech',
-      }
-    ];
     return {
-      news: [news[0], news[1],news[0], news[1],news[0], news[1],news[0], news[1],]
+      pageCount: 0,
+      baseUrl: '//localhost:1337/api',
+      page: 1,
+      limit: 6,
+      news: []
     }
   },
   methods: {
-    clickPageH () {
-
+    clickPageH (n) {
+      this.page = n
+      this.getNews()
+    },
+    getNews () {
+      axios.get(this.baseUrl+'/news', {
+        params: {
+          //locale: 'ru-RU',
+          sort: [ 'createdAt:desc' ],
+          pagination: {
+            start: (this.page-1)*this.limit,
+            limit: this.limit
+          },
+          fields: ['name', 'description', 'createdAt'],
+          populate: {
+            preview_img: {
+              fields: ['name', 'url'],
+            },
+            source: {
+               fields: ['name'],
+            },
+            tags: {
+               fields: ['name'],
+            }
+          }
+        },
+        paramsSerializer: p => {
+          return qs.stringify(p)
+        }
+      })
+        .then(response => {
+          this.news = response.data.data;
+          this.pageCount = Math.ceil(response.data.meta.pagination.total / this.limit);
+        })
+        .catch(response => {
+          console.log(response);
+        })
     }
+  },
+  created () {
+    this.getNews()
   }
 });
 </script>
